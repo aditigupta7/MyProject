2f58482e40e41233a53a6bd216b950d12ccfb2ae
aditigupta7 <32273023+aditigupta7@users.noreply.github.com>
2021-01-18 16:56:32 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import React,{useState} from 'react';
+import {View,Text,SafeAreaView,Button,TextInput} from 'react-native';
+
+
+export default  function App(){
+  const [count,setCount] = useState(0);
+  const [name,setName] =useState('');
+  const [mobile,setMobile] = useState('')
+
+
+  const saveEntry = ()=>{
+    setCount(count+1);
+    setName('');
+    setMobile('');
+
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{backgroundColor:'white' ,padding:20,marginTop:20}}>
+      <View><Text>No. of Entries {count} </Text></View>
+      <View style={{marginVertical:20}}>
+        <TextInput
+          keyboardType="default"
+          maxLength={40}
+          placeholder="Enter Name"
+          placeholderTextColor="black"
+          style ={{paddingVertical:5,paddingHorizontal:10,borderWidth:1,marginVertical:20
+          }}
+          onChangeText ={(text)=> setName(text)}
+          value={name}
+        />
+
+        <TextInput
+          keyboardType="numeric"
+          placeholder="Enter Mobile No."
+          placeholderTextColor="black"
+          style ={{paddingVertical:5,paddingHorizontal:10,borderWidth:1,marginVertical:20
+          }}
+          value={mobile}
+          onChangeText={(text) => setMobile(text)}
+
+        />
+
+        <Button title="Submit"  color="green" onPress={()=> saveEntry()}/>
+      </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
