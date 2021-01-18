b2cb4f8c53598551c7286b5395899825acca8daa
aditigupta7 <32273023+aditigupta7@users.noreply.github.com>
2021-01-18 20:45:54 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,48 +1,100 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Button,
+  TextInput,
+  FlatList,
+  ScrollView,
+  Alert,
+} from 'react-native';
 
-import React,{useState} from 'react';
-import {View,Text,SafeAreaView,Button,TextInput} from 'react-native';
+export default function App() {
+  const [name, setName] = useState('');
+  const [mobile, setMobile] = useState('');
+  const [dataArr, setDataArr] = useState([]);
 
+  const checkPhoneNumber = inputtxt => {
+    var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    return phoneno.test(inputtxt);
+  };
 
-export default  function App(){
-  const [count,setCount] = useState(0);
-  const [name,setName] =useState('');
-  const [mobile,setMobile] = useState('')
+  const saveEntry = () => {
+    if (!name.length) {
+      Alert.alert('', 'Please enter name');
+    } else if (!checkPhoneNumber(mobile)) {
+      Alert.alert('', 'Wrong mobile number');
+    } else {
+      abc();
+    }
+  };
 
+  const abc = () => {
+    let temp = {
+      name: name,
+      phone: mobile,
+    };
+    setDataArr(preVal => {
+      preVal.push(temp);
+      return preVal;
+    });
 
-  const saveEntry = ()=>{
-    setCount(count+1);
     setName('');
     setMobile('');
+  };
 
-  }
-  return (
-    <SafeAreaView style={{backgroundColor:'white' ,padding:20,marginTop:20}}>
-      <View><Text>No. of Entries {count} </Text></View>
-      <View style={{marginVertical:20}}>
-        <TextInput
-          keyboardType="default"
-          maxLength={40}
-          placeholder="Enter Name"
-          placeholderTextColor="black"
-          style ={{paddingVertical:5,paddingHorizontal:10,borderWidth:1,marginVertical:20
-          }}
-          onChangeText ={(text)=> setName(text)}
-          value={name}
-        />
-
-        <TextInput
-          keyboardType="numeric"
-          placeholder="Enter Mobile No."
-          placeholderTextColor="black"
-          style ={{paddingVertical:5,paddingHorizontal:10,borderWidth:1,marginVertical:20
-          }}
-          value={mobile}
-          onChangeText={(text) => setMobile(text)}
-
-        />
-
-        <Button title="Submit"  color="green" onPress={()=> saveEntry()}/>
+  const renderItem = ({item}) => {
+    return (
+      <View style={{borderWidth: 1, marginVertical: 1, paddingHorizontal: 2}}>
+        <Text>name:{item.name}</Text>
+        <Text>mobile:{item.phone}</Text>
       </View>
+    );
+  };
+
+  return (
+    <SafeAreaView
+      style={{backgroundColor: 'white', padding: 20, marginTop: 20}}>
+      <ScrollView>
+        <View>
+          <Text>No. of Entries {dataArr.length} </Text>
+        </View>
+        <FlatList data={dataArr} renderItem={renderItem} />
+        <View style={{marginVertical: 20}}>
+          <TextInput
+            keyboardType="default"
+            maxLength={40}
+            placeholder="Enter Name"
+            placeholderTextColor="black"
+            style={{
+              paddingVertical: 5,
+              paddingHorizontal: 10,
+              borderWidth: 1,
+              marginBottom: 20,
+            }}
+            onChangeText={text => setName(text)}
+            value={name}
+          />
+
+          <TextInput
+            keyboardType="numeric"
+            placeholder="Enter Mobile No."
+            placeholderTextColor="black"
+            style={{
+              paddingVertical: 5,
+              paddingHorizontal: 10,
+              borderWidth: 1,
+              marginVertical: 20,
+            }}
+            value={mobile}
+            maxLength={10}
+            onChangeText={text => setMobile(text)}
+          />
+
+          <Button title="Submit" color="green" onPress={() => saveEntry()} />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
